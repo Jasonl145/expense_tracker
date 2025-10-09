6aeccea04501af2b48302b922b8e22b321e06eb0
Jason Li <jligh145@gmail.com>
2025-10-09 12:18:15 -0400

    initial changes. Create model, expenses list, expenses page

---
+++ lib/expenses.dart
@@ -0,0 +1,35 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  State<Expenses> createState() {
+    return _ExpensesStates();
+  }
+}
+
+class _ExpensesStates extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Cheeseburger",
+      amount: 12.49,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Movie Ticket",
+      amount: 18.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [Text("Chart"), ExpensesList(expenses: _registeredExpenses)]),
+    );
+  }
+}
