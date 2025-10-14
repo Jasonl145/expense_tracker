bd1e9df2e025396f6a81f4d9ed10b6712e183be4
Jason Li <jligh145@gmail.com>
2025-10-14 12:05:15 -0400

    Finish card styling. Built formatter for date

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
+final formatter = DateFormat.yMd();
 enum Category {
   food,
   travel,
   leisure,
   work,
 }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -22,4 +32,9 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
